b7463fdc42140dec9953236c600d8de3ac42bf5a
WhiteInferno <mauro9410@gmail.com>
2021-03-15 03:20:34 -0300

    migrations

--- WC.Context/Migrations/20210315061940_crrections.Designer.cs
+++ WC.Context/Migrations/20210315061940_crrections.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WC.Context;
 
 namespace WC.Context.Migrations
 {
     [DbContext(typeof(WildCrittersDBContext))]
-    partial class WildCrittersDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210315061940_crrections")]
+    partial class crrections
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WC.Context/Migrations/20210315061940_crrections.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WC.Context.Migrations
+{
+    public partial class crrections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "version",
+                table: "NoteVersion",
+                newName: "Version");
+
+            migrationBuilder.RenameColumn(
+                name: "version",
+                table: "Notes",
+                newName: "Version");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "NoteVersion",
+                newName: "version");
+
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "Notes",
+                newName: "version");
+        }
+    }
+}
