7215498c89d66ba273fcde1d22c59a5da9fef76b
WhiteInferno <mauro9410@gmail.com>
2021-04-28 01:44:12 -0300

    adding Alias repo

---
+++ WC.Repository/Contracts/IAliasRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WC.Model;
+
+namespace WC.Repository.Contracts
+{
+    public interface IAliasRepository
+    {
+        IEnumerable<Alias> GetAliases();
+        Alias GetAliasById(int id);
+        void CreateAlias(Alias alias);
+        void DeleteAlias(int aliasId);
+        void UpdateAlias(Alias alias);
+        void Save();
+    }
+}
---
+++ WC.Repository/Implementations/AliasRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WC.Context;
+using WC.Model;
+using WC.Repository.Contracts;
+
+namespace WC.Repository.Implementations
+{
+    public class AliasRepository : IAliasRepository, IDisposable
+    {
+        private readonly WildCrittersDBContext context;
+
+        public AliasRepository(WildCrittersDBContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Alias> GetAliases()
+        {
+            return context.Aliases.ToList();
+        }
+
+        public Alias GetAliasById(int id)
+        {
+            return context.Aliases.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void CreateAlias(Alias alias)
+        {
+            alias.Active = true;
+            alias.DateOfCreation = DateTimeOffset.Now;
+            alias.LastUpdate = DateTimeOffset.Now;
+            context.Aliases.Add(alias);
+        }
+
+        public void DeleteAlias(int aliasId)
+        {
+            Alias alias = context.Aliases.Find(aliasId);
+            context.Aliases.Remove(alias);
+        }
+
+        public void UpdateAlias(Alias alias)
+        {
+            alias.LastUpdate = DateTimeOffset.Now;
+            context.Entry(alias).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
